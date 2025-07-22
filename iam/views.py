826f97d588fb9b2ee2f77eeba2157ed7d7b94379
victor <victornmungai@gmail.com>
2025-07-22 21:17:23 +0300

    Add authentication logic

--- iam/views.py
+++ iam/views.py
@@ -1,11 +1,27 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from iam.serializers import LoginSerializer
 
 
-@api_view(['POST', 'GET'])
+@api_view(['POST'])
 def login(request):
     login_serializer = LoginSerializer(data=request.data)
     login_serializer.is_valid(raise_exception=True)
-    return Response({"message": "Hello there"})
+    # Validate credentials
+    validated_data = login_serializer.validated_data
+    username = validated_data['username']
+    password = validated_data['password']
+
+    valid_user = User.objects.filter(username=username).first()
+    if valid_user:
+        validated_user = authenticate(request, username=username, password=password)
+        if validated_user:
+            # We will add more authentication and authorization condiments later
+            return Response({"message": "Login successful"})
+
+    return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
